b3313b4f60ab55aa203859b2cd6d8afe98baeb47
RandyNyan <149441140+RandyNyan@users.noreply.github.com>
2025-03-25 22:30:09 +0700

    Add files via upload

---
+++ Tugas_Praktikum.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.createElement("div");
+    popup.classList.add("popup-image");
+    popup.innerHTML = '<img src="stiker.jpg" alt="Gambar Keren">';
+    popup.style.display = "none";
+    document.body.appendChild(popup);
+
+    const btnGambar = document.querySelector(".content .btn");
+    btnGambar.addEventListener("click", function() {
+        popup.style.display = (popup.style.display === "none" || popup.style.display === "") ? "block" : "none";
+    });
+
+    popup.addEventListener("click", function() {
+        popup.style.display = "none";
+    });
+});
